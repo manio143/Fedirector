d459d15b4476a98e8be26b5acdd0be77de4856d4
Marian Dziubiak <marian.dziubiak@gmail.com>
2016-07-19 15:24:39 +0200

    Remove semicolons

--- app.fsx
+++ app.fsx
@@ -1,18 +1,18 @@
-#r @"Suave\lib\net40\Suave.dll" ;;
+#r @"Suave\lib\net40\Suave.dll" 
 
-open Suave ;;
-open Suave.Operators ;;
-open Suave.Filters ;;
-open Suave.Redirection ;;
+open Suave
+open Suave.Operators
+open Suave.Filters
+open Suave.Redirection
 
 let app =
     choose
         [ path "/g" >=> redirect "http://www.google.com/" ;
-          path "/md" >=> redirect "http://www.md-techblog.net.pl/" ] ;;
+          path "/md" >=> redirect "http://www.md-techblog.net.pl/" ]
 
 let config = 
     { defaultConfig with
        bindings = [ HttpBinding.mkSimple HTTP "0.0.0.0" 80 ]
-    } ;;
+    }
 
-startWebServer config app ;;
+startWebServer config app
